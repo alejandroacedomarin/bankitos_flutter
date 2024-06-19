d1b7b401740693a490056ee2b43ce52d76bebff2
Titi679 <titicarles@gmail.com>
2024-06-19 16:50:58 +0200

    Notis funcionando

--- lib/Services/SocketService.dart
+++ lib/Services/SocketService.dart
@@ -1,6 +1,8 @@
 import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
-class SocketService {
+class SocketService extends GetxController {
   late IO.Socket socket;
 
   void connectSocket() {
@@ -16,11 +18,32 @@ class SocketService {
     // Escucha cuando el socket se conecta
     socket.on('user-logged-in', (_) {
       print('Usuario conectado');
+      showSnackbar('Usuario conectado');
     });
 
     // Escucha los mensajes del servidor
     socket.on('new-place-created', (newPlace) {
       print('New place created: $newPlace');
+      showSnackbar('Nuevo lugar creado: $newPlace');
     });
+  }
+
+  void showSnackbar(String message) {
+    Get.snackbar(
+      'Bankitos App',
+      message,
+      snackPosition: SnackPosition.TOP,
+      backgroundColor: Colors.orange,
+      colorText: Colors.black,
+      icon: Padding(
+        padding: EdgeInsets.only(left: 12.0),
+        child: Image.asset(
+          'assets/logo.png',
+          width: 24.0,
+          height: 24.0,
+          color: Colors.white, // Color de la imagen si es necesario
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bankitos_flutter/Screens/MainPage/LogIn.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:bankitos_flutter/Services/SocketService.dart';
 
 void main() {
